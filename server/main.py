2bb51397cc611f05df58b095213ab4e252ef0833
Pratik Yesane <pratiky2000@gmail.com>
2025-04-18 21:17:11 +0530

    feat: initialize FastAPI server and added dependencies

--- server/main.py
+++ server/main.py
@@ -1,5 +1,35 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+from config import settings
+import uvicorn
+import os
+
+load_dotenv()
+
+app = FastAPI(
+    title="Scholarship Evaluation API",
+    description="Real-time application processing with Groq",
+    version="1.0.0",
+)
+
+## Main function to run the server
 def main():
-    print("Hello from server!")
+    uvicorn.run(app)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings["ALLOWED_ORIGINS"],  # You can modify this as needed
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/health")
+async def health_check():
+    return {"status": "healthy"}
 
 
 if __name__ == "__main__":
